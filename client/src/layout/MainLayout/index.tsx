76708088a8b839f46e4ae870e6ef461bedb37c53
Davi Fernandes <davifncosta@gmail.com>
2023-12-11 15:59:03 -0300

    feat: add new feature to main layout

--- client/src/layout/MainLayout/index.tsx
+++ client/src/layout/MainLayout/index.tsx
@@ -1,15 +1,36 @@
-import { AddTodo, TodoList } from "../../components";
+import { Dispatch, SetStateAction, useState } from "react";
+import { AddTodo, ToastNotifications, TodoList } from "../../components";
+import type { messageType } from "../../components";
+import "./MainLayout.styles.scss";
 
-import "./MainLayout.styles.scss"
+interface Notification {
+  type: messageType;
+  description: string;
+}
+
+export interface setNotificationProps {
+  setNotifications: Dispatch<SetStateAction<Notification[]>>;
+}
 
 export const MainLayout = () => {
+  const [notifications, setNotifications] = useState<Notification[]>([]);
+
   return (
-    <div className="mainLayout">
-      <h1>To-do Items</h1>
-      <AddTodo />
-      <TodoList/>
-   
-      
-    </div>
+    <>
+      <div className="mainLayout">
+        <h1>To-do Items</h1>
+        <AddTodo setNotifications={setNotifications} />
+        <TodoList setNotifications={setNotifications}/>
+      </div>
+      <div className="wrapper">
+        {notifications.map((notification, index) => (
+          <ToastNotifications
+            key={index}
+            type={notification.type}
+            description={notification.description}
+          />
+        ))}
+      </div>
+    </>
   );
 };
